ebee215911e485021d9367dd01c42fce9f5d81ed
Andreas Rousounelos <andreas.rousounelos@gmail.com>
2024-06-09 00:38:15 +0300

    add vagrant and ansible

---
+++ vagrant/scripts/key.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ ! -f "/home/vagrant/.ssh/id_rsa" ]; then
+    ssh-keygen -t rsa -N "" -f /home/vagrant/.ssh/id_rsa
+fi
+cp /home/vagrant/.ssh/id_rsa.pub /vagrant/control.pub
+
+cat <<SSHEOF > /home/vagrant/.ssh/config
+Host *
+  StrictHostKeyChecking no
+  UserKnownHostsFile=/dev/null
+SSHEOF
+
+chown -R vagrant:vagrant /home/vagrant/.ssh/
